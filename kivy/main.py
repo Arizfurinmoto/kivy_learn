36679fc7ff084911df87966aa8d1629d0fe3aff9
Arizfurinmoto <sz.godon@gmail.com>
2023-02-11 01:25:20 +0100

    Canvas from code

--- kivy/main.py
+++ kivy/main.py
@@ -7,6 +7,8 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 from kivy.metrics import dp
 from kivy.properties import StringProperty, BooleanProperty
+from kivy.graphics.vertex_instructions import Line, Rectangle
+from kivy.graphics.context_instructions import Color
 
 
 class WidgetsExample(GridLayout):
@@ -94,6 +96,17 @@ class CanvasExample2(Widget):
 
 class CanvasExample3(Widget):
     pass
+
+
+class CanvasExample4(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100, 100, 400, 500), width=2)
+            Color(0, 1, 0)
+            Line(circle=(400, 200, 80), width=2)
+            Line(rectangle=(700, 500, 150, 100), width=6)
+            Rectangle(pos=(700, 200), size=(150, 100))
 
 
 TheLabApp().run()
