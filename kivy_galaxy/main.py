063d5c96c327dc7d83802dd56568f552b046b37b
Arizfurinmoto <sz.godon@gmail.com>
2023-03-26 23:33:58 +0200

    infinite generation of tiles

--- kivy_galaxy/main.py
+++ kivy_galaxy/main.py
@@ -34,7 +34,7 @@ class MainWidget(Widget):
     current_speed_x = 0
     current_offset_x = 0
 
-    NB_TILES = 8
+    NB_TILES = 4
     tiles = []
     tiles_coordinates = []
 
@@ -65,8 +65,19 @@ class MainWidget(Widget):
                 self.tiles.append(Quad())
 
     def generate_tiles_coordinates(self):
-        for i in range(0, self.NB_TILES):
-            self.tiles_coordinates.append((0, i))
+        last_y = 0
+
+        for i in range(len(self.tiles_coordinates)-1, -1, -1):
+            if self.tiles_coordinates[i][1] < self.curent_y_loop:
+                del self.tiles_coordinates[i]
+            
+        if len(self.tiles_coordinates) > 0:
+            last_coordinates = self.tiles_coordinates[-1]
+            last_y = last_coordinates[1] + 1
+
+        for i in range(len(self.tiles_coordinates), self.NB_TILES):
+            self.tiles_coordinates.append((0, last_y))
+            last_y += 1
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -149,6 +160,7 @@ class MainWidget(Widget):
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
             self.curent_y_loop += 1
+            self.generate_tiles_coordinates()
 
         #self.current_offset_x += self.current_speed_x*time_factor
 
